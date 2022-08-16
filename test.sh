bc22da27a35e3aeb3c1c022f7c749172b23e7f6b
Nurudeen25 <67113867+Nurudeen25@users.noreply.github.com>
2022-08-16 08:07:28 -0500

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,12 @@
+// this is for Using artifacts
+
+chmod +x ./linux/hello-server
+
+./linux/hello-server &
+
+sleep 3
+
+for i in Anakin Luke Leia Kylo Rey;
+do
+    echo "$(date): $(curl -s http://localhost:11000/${i})"
+done
